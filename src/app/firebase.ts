bf49065a4c86134fdf0554ae38a16a284e8bfa2f
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-21 10:40:43 +0530

    env removed

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,3 +1,8 @@
-import { environment } from '../environments/environment';
-
-export const firebaseConfig = environment.firebaseConfig;
+export const firebaseConfig ={
+    apiKey: "AIzaSyATQsqC2aIcUI8Mq4aXAi0wzhjXasxJHK0",
+      authDomain: "key-manager-15635.firebaseapp.com",
+      projectId: "key-manager-15635",
+      storageBucket: "key-manager-15635.appspot.com",
+      messagingSenderId: "29038001773",
+      appId: "1:29038001773:web:472ced673825bc5f208964"
+}
