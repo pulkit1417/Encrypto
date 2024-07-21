5f7b6ae7b0c65793a33e6f634065f9a4c3372adc
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-21 10:14:01 +0530

    env changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-      projectId: '${PROJECT_ID}',
-      appId: '${APP_ID}',
-      storageBucket: '${STORAGE_BUCKET}',
       apiKey: '${API_KEY}',
       authDomain: '${AUTH_DOMAIN}',
-      messagingSenderId: '${MESSAGING_SENDER_ID}'
+      projectId: '${PROJECT_ID}',
+      storageBucket: '${STORAGE_BUCKET}',
+      messagingSenderId: '${MESSAGING_SENDER_ID}',
+      appId: '${APP_ID}'
     }
   }
